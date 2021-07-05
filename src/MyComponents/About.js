e241695738315466cfb714297e9e2c968723eaf5
Parizat Binta Kabir <43074604+parizat-hridi@users.noreply.github.com>
2021-07-05 17:11:30 +0600

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 export const About = () => {
-
-
-    return (
-        <div>
-            <Card style={{ marginLeft: '75vh', width: '66vh' }}>
-                <Card.Img variant="top" src="https://i.pinimg.com/originals/f6/e9/be/f6e9be9a242d9bcc4b2f7bd0523b68ff.jpg" />
-                <Card.Body>
-                    <Card.Title>Whiteface Mountain</Card.Title>
-                    <Card.Text>
-                        Whiteface Mountain is the fifth-highest mountain in the U.S. state of New York, and one of the High Peaks of the Adirondack Mountains.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-
-        </div>
-    )
-}
+  return (
+    <div>
+      <Card
+        style={{
+          marginLeft: "30%",
+          width: "66vh"
+        }}
+      >
+        <Card.Img
+          variant="top"
+          src="https://i.pinimg.com/originals/f6/e9/be/f6e9be9a242d9bcc4b2f7bd0523b68ff.jpg"
+        />
+        <Card.Body>
+          <Card.Title>Whiteface Mountain</Card.Title>
+          <Card.Text>
+            Whiteface Mountain is the fifth-highest mountain in the U.S. state
+            of New York, and one of the High Peaks of the Adirondack Mountains.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
