2729c916cc63fb9b5e16eb0cf50ad15ae19fadb1
Parizat Binte kabir <parizatbintekabir@gmail.com>
2021-07-05 15:46:59 +0600

    Solo Traveler Website with React

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -6,7 +6,7 @@ export const About = () => {
 
     return (
         <div>
-            <Card style={{ marginLeft: '60vh', width: '70vh' }}>
+            <Card style={{ marginLeft: '75vh', width: '66vh' }}>
                 <Card.Img variant="top" src="https://i.pinimg.com/originals/f6/e9/be/f6e9be9a242d9bcc4b2f7bd0523b68ff.jpg" />
                 <Card.Body>
                     <Card.Title>Whiteface Mountain</Card.Title>
